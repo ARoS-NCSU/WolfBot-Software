a8d9787e5cc793a8d5d593d2c8bd20c0656ce4fc
Neal O'Hara <ngo1991@gmail.com>
2013-07-18 13:32:29 -0400

    changed to form larger triangle

--- agent/demo_2_triangle.py
+++ agent/demo_2_triangle.py
@@ -8,11 +8,11 @@ w = wb.wolfbot()
 #delay each bot by id amout
 #n = w.config['id']
 #time.sleep(n)
-w.move(30,100)
+w.move(30,150)
 time.sleep(1)
-w.move(180,100)
+w.move(180,150)
 time.sleep(1)
-w.move(330,100)
+w.move(330,150)
 
 w.stop()
 

04643edb2ca98c6e9e01b62803030f617b1b4864
Jeff Schornick <jcschorn@ncsu.edu>
2013-07-18 09:39:41 -0400

    Simple wolfbot demo

---
+++ agent/demo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import wolfbot as wb
+import time
+
+w = wb.wolfbot()
+
+#n = w.config['id']
+time.sleep(n)
+w.move(0,100)
+time.sleep(1)
+w.move(90,100)
+time.sleep(1)
+w.move(180,100)
+time.sleep(1)
+w.move(270,100)
+time.sleep(1)
+w.stop()
+

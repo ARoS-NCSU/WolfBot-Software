d301f6f85496b869575c8d4bfb3ba96d9ec326b5
Jeff Schornick <jcschorn@ncsu.edu>
2013-08-09 20:10:19 -0400

    Copy config directory when syncing files

--- os/stage_files.sh
+++ os/stage_files.sh
@@ -2,5 +2,5 @@
 
 ip=${1:-192.168.7.2}
 rsync -vtr fs scripts root@$ip:
-rsync -vtr --delete ../agent ../setup ../util root@$ip:/wolfbot/
+rsync -vtr --delete ../agent ../config ../setup ../util root@$ip:/wolfbot/
 

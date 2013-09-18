e8c5943bdd68bdc3e4a37ced56ef23faa32bf5d6
Jeff Schornick <jcschorn@ncsu.edu>
2013-09-17 20:05:05 -0400

    Report sensor angle in dms_read.sh

--- util/dms_read.sh
+++ util/dms_read.sh
@@ -7,9 +7,9 @@ B=$gpios/gpio3/value
 C=$gpios/gpio2/value
 
 
-for i in "0 0 0" "0 0 1" "0 1 0" "0 1 1" "1 0 0" "1 0 1"; do
-    read c b a <<<$(echo $i)
-    echo -n "$c$b$a : "
+for i in "000 0 0 0" "060 0 0 1" "120 0 1 0" "180 0 1 1" "240 1 0 0" "300 1 0 1"; do
+    read angle c b a <<<$(echo $i)
+    echo -n "$c$b$a ($angle) : "
     echo $a > $A
     echo $b > $B
     echo $c > $C

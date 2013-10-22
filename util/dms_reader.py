879668e95a870c35a5b03344829d1623bd4b51b8
Jeff Schornick <jcschorn@ncsu.edu>
2013-10-21 22:11:50 -0400

    Allow dms_reader to take a count or run continuous

--- util/dms_reader.py
+++ util/dms_reader.py
@@ -5,13 +5,24 @@ sys.path.append('/wolfbot/agent')
 import wolfbot as wb
 import time
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', '--num', type=int, default=1)
+parser.add_argument('-c', '--continuous', action='store_true', default=False)
+args = parser.parse_args()
+
 w = wb.wolfbot()
 
-while True:
-    dms = w.dms_mux.read_all()
-    for name, val in sorted(dms.items()):
-        angle = 360 - name
-        print "name: ", name, "angle: ", angle, "val: ", val
-    print
-    time.sleep(0.1)
+try:
+    count = 0
+    while count < args.num or args.continuous:
+        dms = w.dms_mux.read_all()
+        for name, val in sorted(dms.items()):
+            print "name: {:3}, val: {:4d}".format(name, val)
+        print
+        time.sleep(0.1)
+        count += 1
+except KeyboardInterrupt:
+    pass
 

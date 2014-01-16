913b8345c4d2453bedf0d92e7f994421b610d835
Jeff Schornick <jcschorn@ncsu.edu>
2014-01-16 11:52:30 -0500

    Add salt-ssh bootstrapping configuration

---
+++ bootstrap/install_over_usb.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo Removing old cache files which may have stale configuration
+rm -rf /tmp/salt
+rm -rf /tmp/.salt
+
+echo On failure, check...
+#echo "  msgpack-python is installed on target"
+echo "  salt version >17.4 for files.recurse IOError fix (use development branch if necessary)"
+
+#salt-ssh -ldebug -c saltconf 'bb_usb' state.sls packmaster
+#salt-ssh -c saltconf 'bb_usb' state.sls packmaster
+
+salt-ssh -c salt 'bb_usb' state.sls bootstrap
+

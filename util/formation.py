1cea598c548b66d46f17a8c32b8bb1a865dc59f9
Jeff Schornick <jcschorn@ncsu.edu>
2014-02-22 05:22:05 -0500

    Add option to avoid overlap on looped formations

--- util/formation.py
+++ util/formation.py
@@ -17,6 +17,9 @@ parser.add_argument('-o', '--offset', type=int, default=0)
 parser.add_argument('-c', '--count', type=int, default=11)
 parser.add_argument('-s', '--start', type=float, default=0.0)
 parser.add_argument('-e', '--end', type=float, default=1.0)
+parser.add_argument('-l', '--loop', action='store_true', default=False, 
+        help = 'Augment spacing to avoid overlap in looped formations')
+parser.add_argument('-d', '--debug', action='store_true', default=False)
 args = parser.parse_args()
 
 count = args.count
@@ -27,11 +30,20 @@ name = args.name
 
 wb = wolfbot.wolfbot()
 
-index = ( (wb.config['id']+args.offset) % count)
+
+index = (wb.config['id']+args.offset) % count
 print "Index: #{} from 0-{}".format(index,count-1)
 
 func_range = end-start
-scaled = start + func_range * float(index)/(count-1)
+
+if args.loop:
+    # indexes still 0 - (count-1)
+    # but spread size is now based on count (not count-1)
+    # but we still start at beginning of range
+    #  3 4 5 6 1 2 X  (noone has the highest slot, which overlaps with lowest)
+    scaled = start + func_range * float(index)/count
+else:
+    scaled = start + func_range * float(index)/(count-1)
 print "Scaled: {:0.3} in range [{},{}]".format(scaled,start,end)
 
 formation = importlib.import_module('formations.%s' % name)

6973a4637a0001e58b172e241d0db82a0404009a
Daniel Benavides <dabenavi@ncsu.edu>
2014-02-23 00:38:30 -0500

    bad parens

--- util/formations/circle.py
+++ util/formations/circle.py
@@ -3,8 +3,8 @@ from math import sin, cos, pi
 
 def pose(s, params = [1]):
     r = params[0]
-    x = r*cos(2*pi*(1-s)
-    y = r*sin(2*pi*(1-s)
+    x = r*cos(2*pi*(1-s))
+    y = r*sin(2*pi*(1-s))
     angle = 0
     return x,y,angle
 

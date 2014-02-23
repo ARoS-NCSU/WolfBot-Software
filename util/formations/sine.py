eb6e0355a8cf412859d6fc8c3bd93746a7068cdf
Joseph Betthauser <jlbettha@ncsu.edu>
2014-02-23 15:08:10 -0500

    added cosine and sine poses

---
+++ util/formations/sine.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+from math import sin, cos, pi
+
+def pose(s, params = [1]):
+    r = params[0]
+    x = 2*(s-0.5)
+    y = r*sin(2*pi*(1-s))
+    angle = 0
+    return x,y,angle
+

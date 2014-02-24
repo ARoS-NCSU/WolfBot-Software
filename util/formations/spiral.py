1abe3801726352a709d6df69764f318f55fdc51b
Joseph Betthauser <jlbettha@ncsu.edu>
2014-02-24 14:07:17 -0500

    mod spiral

--- util/formations/spiral.py
+++ util/formations/spiral.py
@@ -4,8 +4,8 @@ from math import sin, cos, pi
 def pose(s, params = [1,1]):
     r = params[0]
     f = params[1]
-    x = r*s*cos(2*pi*f*(s))
-    y = r*s*sin(2*pi*f*(s))
+    x = r*(s-.3)*cos(2*pi*f*(s-.3))
+    y = r*(s-.3)*sin(2*pi*f*(s-.3))
     angle = 0
     return x,y,angle
 

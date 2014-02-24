1a8f34795602eb9684303edd4282fdb450794376
Joseph Betthauser <jlbettha@ncsu.edu>
2014-02-24 13:44:16 -0500

    mod to cosine and sine for freq param

--- util/formations/cosine.py
+++ util/formations/cosine.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 from math import sin, cos, pi
 
-def pose(s, params = [1]):
+def pose(s, params = [0.5,1]):
     r = params[0]
+    f = params[1]
     x = 2*(s-0.5)
-    y = r*cos(2*pi*(1-s))
+    y = r*cos(2*pi*f*(1-s))
     angle = 0
     return x,y,angle
 
